0288a51f5a883482282c0ba2ffd609c200135cda
Dwarven YANG <prison.yang@gmail.com>
2017-07-26 18:38:07 +0800

    reg

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '首页' });
 });
 
 router.get('/login', function(req, res, next) {
@@ -11,7 +11,7 @@ router.get('/login', function(req, res, next) {
 });
 
 router.get('/reg', function(req, res, next) {
-  res.send('reg todo');
+  res.render('reg', { title: '注册' });
 });
 
 module.exports = router;
