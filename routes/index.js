effe1b9700babd16268c16ea3167ead8e898cd32
Dwarven YANG <prison.yang@gmail.com>
2017-07-27 17:54:48 +0800

    post、u

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 var User = require('../models/user.js');
+var Post = require('../models/post.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -88,6 +89,40 @@ router.get('/logout', function(req, res, next) {
   req.session.user = null;
   req.flash('success', '登出成功');
   res.redirect('/');
+});
+
+router.post('/post', checkLogin);
+router.post('/post', function(req, res) {
+  var currentUser = req.session.user;
+  var post = new Post(currentUser.name, req.body.post);
+  post.save(function(err) {
+    if (err) {
+      req.flash('error', err);
+      return res.redirect('/');
+    }
+    req.flash('success', '发表成功');
+    res.redirect('/u/' + currentUser.name);
+  });
+});
+
+router.get('/u/:user', function(req, res) {
+  User.get(req.params.user, function(err, user) {
+    if (user) {
+      Post.get(user.name, function(err, posts) {
+        if (err) {
+          req.flash('error', err);
+          return res.redirect('/');
+        }
+        res.render('user', {
+          title: user.name,
+          posts: posts,
+        });
+      });
+    } else {
+      req.flash('error', '用户不存在');
+      return res.redirect('/');
+    }
+  })
 });
 
 function checkLogin(req, res, next) {
